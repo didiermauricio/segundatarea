12681b8b240f023b4833eea75a8139b890fe192d
didiermauricio <didiermauricio@gmail.com>
2016-11-11 21:28:21 +0100

    First Version

---
+++ SecondActivityAndroid/app/src/main/java/com/example/didier/secondactivityandroid/HomeActivity.java
@@ -0,0 +1,73 @@
+package com.example.didier.secondactivityandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import com.example.sqlitedemoo.R;
+
+public class HomeActivity extends Activity {
+
+    Button addmem_bt;
+    ListView lv;
+    SQLController dbcon;
+    TextView memID_tv, memName_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        dbcon = new SQLController(this);
+        dbcon.open();
+        addmem_bt = (Button) findViewById(R.id.newTask);
+        lv = (ListView) findViewById(R.id.task_id);
+        addmem_bt.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent add_mem = new Intent(HomeActivity.this, NewTaskActivity.class);
+                startActivity(add_mem);
+            }
+        });
+
+        // Attach The Data From DataBase Into ListView Using Crusor Adapter
+        Cursor cursor = dbcon.readData();
+        String[] from = new String[] { DBhelper.MEMBER_ID, DBhelper.MEMBER_NAME };
+        int[] to = new int[] { R.id.member_id, R.id.member_name };
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                HomeActivity.this, R.layout.view_task_sumary, cursor, from, to);
+
+        adapter.notifyDataSetChanged();
+        lv.setAdapter(adapter);
+
+        // OnCLickListiner For List Items
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                memID_tv = (TextView) view.findViewById(R.id.member_id);
+                memName_tv = (TextView) view.findViewById(R.id.member_name);
+
+                String memberID_val = memID_tv.getText().toString();
+                String memberName_val = memName_tv.getText().toString();
+
+                Intent modify_intent = new Intent(getApplicationContext(),
+                        ModifyTask.class);
+                modify_intent.putExtra("memberName", memberName_val);
+                modify_intent.putExtra("memberID", memberID_val);
+                startActivity(modify_intent);
+            }
+        });
+
+    } // create end
+
+}// class end
---
+++ SecondActivityAndroid/app/src/main/java/com/example/didier/secondactivityandroid/NewTaskActivity.java
@@ -0,0 +1,51 @@
+package com.example.didier.secondactivityandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.sqlitedemoo.R;
+
+public class NewTaskActivity extends Activity implements OnClickListener {
+	EditText et;
+	Button add_bt, read_bt;
+	SQLController dbcon;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_newtask);
+		et = (EditText) findViewById(R.id.taskTitle);
+		add_bt = (Button) findViewById(R.id.saveTask);
+
+		dbcon = new SQLController(this);
+		dbcon.open();
+		add_bt.setOnClickListener(this);
+	}
+	public void onButtonClicked(View v){
+		DatePickerFragment newFragment = new DatePickerFragment();
+		newFragment.show(getFragmentManager(),"Date Picker");
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.saveTask:
+			String name = et.getText().toString();
+			dbcon.insertData(name);
+			Intent main = new Intent(NewTaskActivity.this, HomeActivity.class)
+					.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(main);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
